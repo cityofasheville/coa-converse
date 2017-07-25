08c82bdef389b6461d74c0001cd7d186d0eda24d
fruiz7 <frances.ruiz@gmail.com>
2017-07-25 14:37:33 -0400

    add temporarily server link

--- src/utilities/gqlClient.js
+++ src/utilities/gqlClient.js
@@ -2,7 +2,8 @@ import ApolloClient, { createNetworkInterface } from 'apollo-client';
 
 
 //const SERVER_URL = 'https://coa-converse-api.ashevillenc.gov/graphql';
-const SERVER_URL = 'http://localhost:8080/graphql';
+// const SERVER_URL = 'http://localhost:8080/graphql';
+const SERVER_URL = 'http://72.250.240.49/graphql';
 
 const networkInterface = createNetworkInterface({ uri: SERVER_URL });
 

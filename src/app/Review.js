76ab49f6c967edd93b0b74644ad4ac716a3f62b9
fruiz7 <frances.ruiz@gmail.com>
2017-09-18 09:53:32 -0400

    closes #13 Add guiding commentary expandable panel

--- src/app/Review.js
+++ src/app/Review.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import Modal from 'react-modal';
+import Collapsible from 'react-collapsible';
 import { Link, browserHistory } from 'react-router';
 import { graphql } from 'react-apollo';
 import gql from 'graphql-tag';
@@ -245,6 +246,35 @@ class Review extends React.Component {
                       </div>
                     </div>
                   </fieldset>
+                </div>
+                <div className="form-group">
+                  <Collapsible trigger="Click here for your Check-in Reminder">
+                    <div>
+                      <p>
+                        The purpose of the check-in is to provide an opportunity for regular conversations between supervisors and employees. During a check-in employees and supervisors can set or clarify expectations, share observations, and discuss career aspirations and development needs that will enhance the employee&apos;s performance for success.
+                      </p>
+                      <p>
+                        Check-in core areas:
+                        <ol>
+                          <li>Observations and Reflections: Share frequent, two-way feedback between employee and supervisor, providing observations and reflections about current expectations, responsibilities and goals.</li>
+                          <li>Looking Forward: Identify any new or revised expectations, goals, responsibilities or special projects. A new project may be identified based on the discussion.</li>
+                          <li>Career: Discuss any career goals or aspirations, and/or how the employee can continue to grow in their role.</li>
+                          <li>Development: The employee and supervisor can discuss and set actionable goals for professional development.</li>
+                        </ol>
+                      </p>
+                      <p>
+                        Best Practices:
+                        <ul>
+                          <li>Provide timely, specific, balanced observations.</li>
+                          <li>Actively listen; listen to understand.</li>
+                          <li>Ask questions to ensure you are clear about the feedback you&apos;re receiving.</li>
+                          <li>Value different perspectives and be open to hearing constructive feedback.</li>
+                          <li>Create mutually agreed upon actions and time lines.</li>
+                          <li>Be sure to talk about successes and accomplishments as well as any areas where improvement is necessary.</li>
+                        </ul>
+                      </p>
+                    </div>
+                  </Collapsible>
                 </div>
                 <div className="form-group">
                   {this.props.review.questions.map((question, index) => (

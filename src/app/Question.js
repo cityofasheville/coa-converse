5da6ff063fde8191181917b2bbe741f4ed36154c
fruiz7 <frances.ruiz@gmail.com>
2018-03-28 16:37:49 -0400

    Fix issue with responses only allowing one letter at a time (#57)

--- src/app/Question.js
+++ src/app/Question.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import { RadioGroup, Radio } from 'react-radio-group';
 import PropTypes from 'prop-types';
-import Response from './Response';
 import RichTextEditor from '../shared/RichTextEditor';
 
 const markup = (stringValue) => {

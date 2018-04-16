0d1a056d0c48da088cdb12bf76889b20d124147c
fruiz7 <frances.ruiz@gmail.com>
2018-04-16 16:26:30 -0400

    Add timestamp to error messages for easier debugging (#61)

--- src/shared/Error.js
+++ src/shared/Error.js
@@ -1,11 +1,15 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import moment from 'moment';
 
 const Error = props => (
   <div className="alert alert-danger alert-sm">
     <p>
       There was an error reaching the server. Please contact the Help Desk at x4000 or <a href="mailto:help@ashevillenc.gov" target="_blank" style={{ color: '#fff', textDecoration: 'underline' }}>help@ashevillenc.gov</a>.
     </p>
+    <p>
+      Time: {moment().format('M/DD/YYYY HH:mm:ss Z')} UTC
+    </p>    
     <p>
       <span>Error details: </span>{props.message}
     </p>

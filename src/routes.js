b33987b955b5ba153cab167b1975a30a8c7e9053
fruiz7 <frances.ruiz@gmail.com>
2018-08-06 15:51:02 -0400

    AVL-RN: Add Google Analytics

--- src/routes.js
+++ src/routes.js
@@ -2,8 +2,12 @@ import React from 'react';
 import { Router, Route, IndexRoute, browserHistory } from 'react-router';
 import { ApolloProvider } from 'react-apollo';
 
+
 // GraphQL Client
 import { client } from './gqlClient';
+
+// Google Analytics
+import ReactGA from 'react-ga';
 
 // Routed components
 import App from './app/App';
@@ -11,9 +15,24 @@ import Home from './app/Home';
 import Reviews from './app/Reviews';
 import ReviewContainer from './app/ReviewContainer';
 
+let logPageView = () => {
+  ReactGA.set({ page: window.location.pathname });
+  ReactGA.pageview(window.location.pathname + window.location.search);
+};
+
+if (window.location.href.indexOf('dev') === -1
+  && (window.location.href.indexOf('check-in.ashevillenc.gov') > -1 ||
+  window.location.href.indexOf('checkin.ashevillenc.gov') > -1 ||
+  window.location.href.indexOf('checkins.ashevillenc.gov') > -1)
+) {
+  ReactGA.initialize('UA-16340971-15');
+} else {
+  logPageView = null;
+}
+
 const Routes = () => (
   <ApolloProvider client={client} >
-    <Router history={browserHistory}>
+    <Router history={browserHistory} onUpdate={logPageView === null ? null : () => logPageView()}>
       <Route path="/" component={App}>
         <IndexRoute component={Home} />
         <Route path="check-ins" component={Reviews}></Route>

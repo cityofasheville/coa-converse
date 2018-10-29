914f1bfed6da1545aa05441046e9aa248e160035
fruiz7 <frances.ruiz@gmail.com>
2018-10-29 15:37:24 -0400

    Fix CORS error on TinyMCE

--- src/index.js
+++ src/index.js
@@ -2,8 +2,24 @@ import React from 'react';
 import { render } from 'react-dom';
 import 'babel-polyfill';
 
+// Import TinyMCE
+import tinymce from 'tinymce/tinymce';
+
+// A theme is also required
+import 'tinymce/themes/modern/theme';
+
+// Any plugins you want to use has to be imported
+import 'tinymce/plugins/paste';
+import 'tinymce/plugins/link';
+
 // Import Routes
 import Routes from './routes';
+
+// Initialize the app
+tinymce.init({
+  selector: '#tiny',
+  plugins: ['paste', 'link', 'image', 'code', 'lists']
+});
 
 // Import styles
 require('./styles/styles.scss');
